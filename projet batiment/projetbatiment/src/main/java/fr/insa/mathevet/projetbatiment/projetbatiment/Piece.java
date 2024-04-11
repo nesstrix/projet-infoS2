d69d2a1f0df96766611268917b46bb80a6e02ddd
user <user@localhost>
2024-04-11 15:46:30 +0200

    mur

--- projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/Piece.java
+++ projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/Piece.java
@@ -4,6 +4,9 @@
  */
 package fr.insa.mathevet.projetbatiment.projetbatiment;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.LinkedList;
 /**
  *
  * @author emma0
@@ -15,6 +18,8 @@ public class Piece {
     private Plafond plafond;
     private Coin ca;
     private Coin cb;
+    private Coin cc;
+    private Coin cd;
     private Sol surfacesol;
     private double hauteur;
     
@@ -25,23 +30,31 @@ public class Piece {
     private List<Mur> listMurs;
     
     
-    ArrayList<Mur> listMurs=new ArrayList<Mur>();
 
-    public Piece(int idPiece, Sol sol, Plafond plafond, Coin ca, Coin cb, Sol surfacesol, double hauteur, Mur mur1, Mur mur2, Mur mur3, Mur mur4, <any> listMurs) {
+    public Piece(int idPiece, Coin ca, Coin cb, Coin cc, Coin cd, Sol surfacesol, double hauteur) {
         this.idPiece = idPiece;
         this.sol = sol;
         this.plafond = plafond;
         this.ca = ca;
         this.cb = cb;
+        this.cc = cc;
+        this.cd = cd;
         this.surfacesol = surfacesol;
         this.hauteur = hauteur;
-        this.mur1 = mur1;
-        this.mur2 = mur2;
-        this.mur3 = mur3;
-        this.mur4 = mur4;
-        this.listMurs = listMurs;
+        this.mur1 = new Mur(1, ca, cc,3);
+        this.mur2 = new Mur (2, cc, cb, 3);
+        this.mur3 = new Mur (3, cb, cd, 3);
+        this.mur4 = new Mur (4, cd, ca, 3);
+        //this.listMurs = new Arraylist <mur1, mur2, mur3, mur4>;
+        //this.listMurs = new ArrayList<>(Arrays.asList(mur1, mur2, mur3, mur4));
+        List<Mur> listMurs=new ArrayList<>();
+        listMurs.add(mur1);
+        listMurs.add(mur2);
+        listMurs.add(mur3);
+        listMurs.add(mur4);
     }
-
+    
+ 
     public Sol getSurfacesol() {
         return surfacesol;
     }
