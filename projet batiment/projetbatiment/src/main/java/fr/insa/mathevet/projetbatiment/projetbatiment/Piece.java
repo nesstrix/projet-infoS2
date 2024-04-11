11488f7a80c290ec6222c65c503ea2ba2034bb8c
user <user@localhost>
2024-04-11 13:51:07 +0200

    mur

--- projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/Piece.java
+++ projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/Piece.java
@@ -11,17 +11,52 @@ import java.util.ArrayList;
 public class Piece {
     
     private int idPiece;
-    private int sol;
-    private int plafond;
+    private Sol sol;
+    private Plafond plafond;
+    private Coin ca;
+    private Coin cb;
+    private Sol surfacesol;
+    private double hauteur;
+    
+    private Mur mur1;
+    private Mur mur2;
+    private Mur mur3;
+    private Mur mur4;
+    private List<Mur> listMurs;
+    
+    
     ArrayList<Mur> listMurs=new ArrayList<Mur>();
 
-    public Piece(int idPiece, int sol, int plafond) {
+    public Piece(int idPiece, Sol sol, Plafond plafond, Coin ca, Coin cb, Sol surfacesol, double hauteur, Mur mur1, Mur mur2, Mur mur3, Mur mur4, <any> listMurs) {
         this.idPiece = idPiece;
         this.sol = sol;
         this.plafond = plafond;
+        this.ca = ca;
+        this.cb = cb;
+        this.surfacesol = surfacesol;
+        this.hauteur = hauteur;
+        this.mur1 = mur1;
+        this.mur2 = mur2;
+        this.mur3 = mur3;
+        this.mur4 = mur4;
+        this.listMurs = listMurs;
     }
 
-    public double surfaceSol (){
+    public Sol getSurfacesol() {
+        return surfacesol;
+    }
+
+    public void setSurfacesol(Sol surfacesol) {
+        this.surfacesol = surfacesol;
+    }
+
+
+    
+    
+
+
+
+    
         
     }
     @Override
