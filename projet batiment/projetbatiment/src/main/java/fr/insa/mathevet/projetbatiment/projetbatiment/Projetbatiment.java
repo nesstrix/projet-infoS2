b5df20d22c0e024e9908a6ef9cfbf25451e654f9
user <user@localhost>
2024-04-23 15:58:16 +0200

    mur

--- projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/Projetbatiment.java
+++ projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/Projetbatiment.java
@@ -15,34 +15,44 @@ public class Projetbatiment {
     public static void main(String[] args) {
 
       
-      
-      System.out.println("combien de murs voulez vous ?");
-      int nbMurs = Lire.i();
-      int compteurMur = 0;
-      int compteurCoin = 0;
-      for (int i=0; i < nbMurs; i++){
-          int MurId = ++compteurMur;
-          System.out.println ("quel est votre coin debut ?");
-          int Coindebut = ++compteurCoin;
-          double DebutX = Lire.d();
-          double DebutY = Lire.d();
-          Coin CoinDeb = new Coin(Coindebut, DebutX, DebutY);
-          System.out.println("quel est votre coin fin ?");
-          int Coinfin = ++compteurCoin;
-          double FinX = Lire.d();
-          double FinY = Lire.d();
-          Coin CoinFin = new Coin(Coinfin, FinX, FinY);
-          System.out.println("combien voulez-vous de portes ?");
-          int nbrePortes = Lire.i();
-          System.out.println("combien de fenêtres voulez-vous ?");
-          int nbreFenetres = Lire.i();
-          Mur Mur1 = new Mur(MurId, CoinDeb, CoinFin, 3.0);
-          double surfacefenêtre = Mur1.surfaceFenetres(nbreFenetres);
-          System.out.println("la surface fenêtre est : " + surfacefenêtre);
-          double surfaceporte = Mur1.surfacePortes(nbrePortes);
-          System.out.println("la surface porte est : " + surfaceporte);
-          double surfacemur = Mur1.surface(surfaceporte, surfacefenêtre);
-          System.out.println("la surface du mur est : " + surfacemur);
+      System.out.println ("combien de pièces voulez-vous ?");
+      int nbPieces = Lire.i();
+      int compteurPiece = 0;
+      for (int j=0; j < nbPieces; j++){
+          System.out.println("combien de murs voulez vous ?");
+          int nbMurs = Lire.i();
+          int compteurMur = 0;
+          int compteurCoin = 0;
+          for (int i=0; i < nbMurs; i++){
+              int MurId = ++compteurMur;
+              System.out.println ("quel est votre coin debut ?");
+              int Coindebut = ++compteurCoin;
+              double DebutX = Lire.d();
+              double DebutY = Lire.d();
+              Coin CoinDeb = new Coin(Coindebut, DebutX, DebutY);
+              System.out.println("quel est votre coin fin ?");
+              int Coinfin = ++compteurCoin;
+              double FinX = Lire.d();
+              double FinY = Lire.d();
+              Coin CoinFin = new Coin(Coinfin, FinX, FinY);
+              System.out.println("combien voulez-vous de portes ?");
+              int nbrePortes = Lire.i();
+              System.out.println("combien de fenêtres voulez-vous ?");
+              int nbreFenetres = Lire.i();
+              Mur Mur1 = new Mur(MurId, CoinDeb, CoinFin, 3.0);
+              double surfacefenêtre = Mur1.surfaceFenetres(nbreFenetres);
+              System.out.println("la surface fenêtre est : " + surfacefenêtre);
+              double surfaceporte = Mur1.surfacePortes(nbrePortes);
+              System.out.println("la surface porte est : " + surfaceporte);
+              double surfacemur = Mur1.surface(surfaceporte, surfacefenêtre);
+              System.out.println("la surface du mur est : " + surfacemur);
+          }
+          Piece Piece1 = new Piece(PieceId, sol, plafond, )
+              double surfacepiece = Piece1.surfacesol ()
+              System.out.println ("la surface au sol de la piece est :" + surfacepiece);
+          
+      }
+
 
           
           
