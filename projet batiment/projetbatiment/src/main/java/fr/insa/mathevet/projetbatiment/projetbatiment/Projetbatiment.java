fee4a77888033d9dd725c73fc59e7f0700e9d16a
emma0 <emma0@172.29.117.215>
2024-04-30 19:59:31 +0200

    C'ESTTTT BEAUUUUUUUUUUUU

--- projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/Projetbatiment.java
+++ projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/Projetbatiment.java
@@ -7,116 +7,124 @@ package fr.insa.mathevet.projetbatiment.projetbatiment;
 /**
  *
  * @author emma0
+ * 
+ * 
  */
-import fr.insa.mathevet.projetbatiment.projetbatiment.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Projetbatiment {
 
     public static void main(String[] args) {
+       
         
-      System.out.println("Voulez-vous une maison ou un appartement?");
-      System.out.println ("si vous voulez une maison répondre 1, si vous voulez un immeuble répondre 2");
-      int l = Lire.i();
-      if (l == 1) {
-          System.out.println ("combien de niveau voulez-vous?");
-          int n = Lire.i();
-      } else {
-          System.out.println("combien d'appartement voulez-vous?");
-          int a = Lire.i();
-      }
-      //for (int i=0; i < n; i++ ){
-          
-      //}
-      System.out.println ("combien de pièces voulez-vous ?");
-      int nbPieces = Lire.i();
-      int compteurPiece = 0;
+        // Chargement des revêtements depuis le fichier
+        uniRevetement[] revetements = Revetement.remplirRevetements();
+
+        System.out.println("Voulez-vous construire une maison ou un immeuble?");
+        System.out.println("Si vous voulez une maison, répondez 1, si vous voulez un immeuble, répondez 2");
+        int typeConstruction = Lire.i();
+        int nbNiveaux = 0;
+        int compteurPiece = 0;
       int compteurSol = 0;
       int compteurPlafond = 0;
       int compteurMur = 0;
       int compteurCoin = 0;
-      for (int j=0; j < nbPieces; j++){
-          int idPiece= ++compteurPiece;  
-          
-          System.out.println ("quel est votre premier coin ?");
-          int Coindebut = ++compteurCoin;
-          double DebutX = Lire.d();
-          double DebutY = Lire.d();
-          Coin Coin1 = new Coin(Coindebut, DebutX, DebutY);
-          
-          System.out.println("quel est votre deuxième coin ?");
-          int Coinmid = ++compteurCoin;
-          double midX = Lire.d();
-          double midY = Lire.d();
-          Coin Coin2 = new Coin(Coinmid, midX, midY);
-          
-          System.out.println("quel est votre troisième coin ?");
-          int Coinfin = ++compteurCoin;
-          double FinX = Lire.d();
-          double FinY = Lire.d();
-          Coin Coin3 = new Coin(Coinfin, FinX, FinY);
-          
-          List<Coin> listCoin = new ArrayList <>();
-          listCoin.add(Coin1);
-          listCoin.add(Coin2);
-          listCoin.add(Coin3);
-          
-          int MurId = ++compteurMur;
-          System.out.println("combien voulez-vous de portes pour le mur ?");
-          int nbrePortes = Lire.i();
-          System.out.println("combien de fenêtres voulez-vous pour le mur ?");
-          int nbreFenetres = Lire.i();
-          Mur Mur1 = new Mur(MurId, Coin1, Coin2, 3.0);
-          
-          double surfacefenêtre = Mur1.surfaceFenetres(nbreFenetres);
-          System.out.println("la surface fenêtre est : " + surfacefenêtre);
-          double surfaceporte = Mur1.surfacePortes(nbrePortes);
-          System.out.println("la surface porte est : " + surfaceporte);
-          double longueur1 = Mur1.Longueur(Coin1, Coin2);
-          System.out.println("la longueur du mur " + MurId + " est : " + longueur1);
-          double surfacemur1 = Mur1.surface(surfaceporte, surfacefenêtre , longueur1);
-          System.out.println("la surface du mur " + MurId + " est : " + surfacemur1);
-          
-          List<Mur> listMurs = new ArrayList <>();
-          listMurs.add(Mur1);
-          
-          MurId = ++compteurMur;
-          System.out.println("combien voulez-vous de portes pour le mur ?");
-          int nbrePortes2 = Lire.i();
-          System.out.println("combien de fenêtres voulez-vous pour le mur ?");
-          int nbreFenetres2 = Lire.i();
-          Mur Mur2 = new Mur(MurId, Coin2, Coin3, 3.0);
-          
-          double surfacefenêtre2 = Mur2.surfaceFenetres(nbreFenetres2);
-          System.out.println("la surface fenêtre est : " + surfacefenêtre2);
-          double surfaceporte2 = Mur2.surfacePortes(nbrePortes2);
-          System.out.println("la surface porte est : " + surfaceporte2);
-          double longueur2 = Mur2.Longueur(Coin2, Coin3);
-          System.out.println("la longueur du mur " + MurId + " est : " + longueur2);
-          double surfacemur2 = Mur2.surface(surfaceporte2, surfacefenêtre2, longueur2);
-          System.out.println("la surface du mur " + MurId + " est : " + surfacemur2);
-         
-          listMurs.add(Mur2);
-          
-          int sol = ++compteurSol ;
-          int plafond = ++compteurPlafond ;
-          Sol Sol1 = new Sol (sol);
-          double surfacesol = longueur1 * longueur2;
-          System.out.println("la surface du sol " + sol + " est :" + surfacesol);
-          System.out.println("la surface du plafond " + plafond + " est :" + surfacesol);
-          Piece Piece1 = new Piece(idPiece, sol, plafond, 3.0, listMurs);
-          //double surfacepiece = Piece1.Surfacesol(Coin1, Coin3);
-          System.out.println ("la surface au sol de la piece " + idPiece + " est :" + surfacesol);
-          
-          List<Piece> listPieces = new ArrayList <>();
-          listPieces.add(Piece1);
-          System.out.println(listCoin);
-          //System.out.println(listMurs);
-          System.out.println(listPieces);
-          
-      }
+
+        if (typeConstruction == 1) {
+            System.out.println("Combien de niveaux voulez-vous pour la maison?");
+            nbNiveaux = Lire.i();
+        } else {
+            System.out.println("Combien d'appartements voulez-vous dans l'immeuble?");
+            nbNiveaux = Lire.i();  //1 niveau = 1 appart
+        }
+
+        System.out.println("Combien de pièces voulez-vous?");
+        int nbPieces = Lire.i();
+
+        List<Piece> listPieces = new ArrayList<>();
+        
+        for (int i = 0; i < nbPieces; i++) {
+            System.out.println("Configuration de la pièce numéro " + (i + 1));
+            List<Mur> murs = new ArrayList<>();
+
+            for (int j = 0; j < 4; j++) {
+                System.out.println("Mur " + (j + 1) + ": Entrez les coordonnées de début (x puis y):");
+                int Coindebut = ++compteurCoin;
+                double debutX = Lire.d();
+                double debutY = Lire.d();
+                Coin debut = new Coin(Coindebut, debutX, debutY);
+
+                System.out.println("Entrez les coordonnées de fin (x puis y):");
+                int Coinfin = ++compteurCoin;
+                double finX = Lire.d();
+                double finY = Lire.d();
+                Coin fin = new Coin(Coinfin, finX, finY);
+
+                System.out.println("Nombre de portes sur ce mur:");
+                int nbPortes = Lire.i();
+
+                System.out.println("Nombre de fenêtres sur ce mur:");
+                int nbFenetres = Lire.i();
+
+                
+                Mur mur = new Mur(j, debut, fin, 2.5);  
+                mur.setNbrePortes(nbPortes);
+                mur.setNbreFenetres(nbFenetres);
+
+                
+                System.out.println("Options de revêtement pour ce mur:");
+                for (uniRevetement rev : revetements) {
+                    if (rev.isPourMur()) {
+                        System.out.println("ID: " + rev.getId() + " - " + rev.getNom() + " à " + rev.getPrixm2() + "€ par m²");
+                    }
+                }
+                
+                System.out.println("Entrez l'ID du revêtement que vous souhaitez pour ce mur:");
+                int idRevetement = Lire.i();
+                uniRevetement revetementChoi = null ;
+
+               
+                for (uniRevetement rev : revetements) {
+                    if (rev.getId() == idRevetement && rev.isPourMur()) {
+                        mur.setRevetement(rev);
+                        revetementChoi = rev ;
+                        System.out.println("Revêtement choisi : " + rev.getNom());
+                        
+                    }
+                }
+                        if (revetementChoi != null){
+                            double longueur =mur.Longueur(debut, fin) ;
+                            double surfacePortes = mur.surfacePortes(nbPortes);
+                            double surfaceFenetres = mur.surfaceFenetres(nbFenetres);
+                            double surfaceMur = mur.surface(surfacePortes, surfaceFenetres, longueur);
+                            double cout = surfaceMur * revetementChoi.getPrixm2();
+                             System.out.println("Surface du mur après déductions : " + surfaceMur + " m²");
+                    System.out.println("Coût pour revêtir le mur : " + cout + "€");
+                    murs.add(mur);
+                } else {
+                    System.out.println("Aucun revêtement valide sélectionné pour ce mur.");
+                }
+                        
+                // Calcul et affichage de la surface et du coût
+                double longueur = mur.Longueur(debut, fin);
+                double surfacePortes = mur.surfacePortes(nbPortes);
+                double surfaceFenetres = mur.surfaceFenetres(nbFenetres);
+                double surfaceMur = mur.surface(surfacePortes, surfaceFenetres, longueur);
+                System.out.println("Surface du mur après déductions : " + surfaceMur + " m²");
+                murs.add(mur);
+            }
+
+            // Création de la pièce avec les murs configurés
+            Piece piece = new Piece(i, 0, 0, 2.5, murs);  // IDs sol et plafond non utilisés pour simplification
+            listPieces.add(piece);
+        }
+
+        
+        System.out.println("Voici les pièces créées :");
+        for (Piece piece : listPieces) {
+            System.out.println(piece);
+        }
     }
 }
