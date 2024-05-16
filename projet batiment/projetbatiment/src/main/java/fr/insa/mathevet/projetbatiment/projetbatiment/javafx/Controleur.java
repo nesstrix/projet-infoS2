256579200916884f2ca4e93d4d10ebc0c5a83b92
user <user@localhost>
2024-05-16 23:38:51 +0200

    mur

--- projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/javafx/Controleur.java
+++ projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/javafx/Controleur.java
@@ -4,6 +4,11 @@
  */
 package fr.insa.mathevet.projetbatiment.projetbatiment.javafx;
 
+import fr.insa.mathevet.projetbatiment.projetbatiment.Coin;
+import fr.insa.mathevet.projetbatiment.projetbatiment.Projetbatiment;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
 /**
  *
  * @author user
@@ -12,9 +17,27 @@ public class Controleur {
     
     private MainPain vue;
     
+    private int etat; 
+    
     public Controleur (MainPain vue){
         this.vue = vue;
+        this.changeEtat(30);
        
     }
     
+    public void changeEtat(int nouvelEtat) {
+        if (nouvelEtat == 30)
+            this.vue.getB().setDisable(true);  
+            this.vue.getC().setDisable(true);
+        
+    }
+
+    void clicDansZoneDessin(MouseEvent t) {
+        double id = t.getId();
+        double px = t.getX();
+        double py = t.getY();
+        Color col= Color.color(Math.random(), Math.random(), Math.random());
+        Projetbatiment model = this.vue.getModel();
+        model.add(new Coin(id, px ,py));
+    }
 }
