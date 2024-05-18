ee9b1b40422ff20b4ddde4241a4e745c95481289
user <user@localhost>
2024-05-18 23:03:45 +0200

    c'est bon !!

--- projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/javafx/Projetbatiment.java
+++ projet batiment/projetbatiment/src/main/java/fr/insa/mathevet/projetbatiment/projetbatiment/javafx/Projetbatiment.java
@@ -16,11 +16,8 @@ import fr.insa.mathevet.projetbatiment.projetbatiment.Mur;
 import fr.insa.mathevet.projetbatiment.projetbatiment.Piece;
 import fr.insa.mathevet.projetbatiment.projetbatiment.Revetement;
 import fr.insa.mathevet.projetbatiment.projetbatiment.Sol;
-import fr.insa.mathevet.projetbatiment.projetbatiment.javafx.Pan2D;
-import fr.insa.mathevet.projetbatiment.projetbatiment.uniRevetement;
 import fr.insa.mathevet.projetbatiment.projetbatiment.uniRevetement;
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileWriter;
@@ -28,28 +25,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-//import javafx.scene.control.RadioButton;
 
-
-
-public class Projetbatiment extends JFrame {  
-    /*au moment de l'interface nous avions du mal à créer une interface parce qu'on 
-    arrivait pas à la faire coincider avec le main "original" (par la console) de notre 
-    interface, ça nous paraissait donc plus simple d'effectuer l'interface directement 
-    sur notre main, nous trouvions ça plus simple */
-
-    private JComboBox<String> typeconstruc;
+public class Projetbatiment extends JFrame {
     private JTextField niveauxField;
     private int nbNiveaux = 0;
     private double coutTotal = 0;
     private HashMap<String, Double> ctparRevetement = new HashMap<>();
     private HashMap<String, Double> stparRevetement = new HashMap<>();
-
     private List<Piece> pieces = new ArrayList<>();
     private JRadioButton rbMaison;
     private JRadioButton rbImmeuble;
 
-    public Projetbatiment() {  // création de la première page 
+    public Projetbatiment() {
         setTitle("Projet Batiment");
         setSize(400, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -61,7 +48,6 @@ public class Projetbatiment extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        
         panel.add(new JLabel("Voulez-vous construire une maison ou un immeuble"));
         rbMaison = new JRadioButton("Maison");
         rbImmeuble = new JRadioButton("Immeuble");
@@ -73,32 +59,31 @@ public class Projetbatiment extends JFrame {
         radioPanel.add(rbImmeuble);
 
         panel.add(radioPanel);
-        
 
         panel.add(new JLabel("Combien de niveaux voulez-vous? "));
-        niveauxField = new JTextField();   //grâce à JTextField on peut écrire dans l'interface 
+        niveauxField = new JTextField();
         panel.add(niveauxField);
 
         JButton startButton = new JButton("Commencer");
-        startButton.addActionListener(new StartButtonListener());   //on peut mtn detecter si il est cliqué ou non
+        startButton.addActionListener(new StartButtonListener());
         panel.add(startButton);
 
         JButton saveButton = new JButton("Sauvegarder Devis");
-        saveButton.addActionListener(new SaveButtonListener());     //on peut mtn detecter si il est cliqué ou non
+        saveButton.addActionListener(new SaveButtonListener());
         panel.add(saveButton);
 
         JButton drawButton = new JButton("Plan 2D");
-        drawButton.addActionListener(new DrawButtonListener());     //on peut mtn detecter si il est cliqué ou non
+        drawButton.addActionListener(new DrawButtonListener());
         panel.add(drawButton);
 
         add(panel);
     }
 
-    private class StartButtonListener implements ActionListener {           //"ecouter" le bouton
+    private class StartButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String type;       //on récupère ce que l'utilisateur a choisi depuis l'interface
-             if (rbMaison.isSelected()) {
+            String type;
+            if (rbMaison.isSelected()) {
                 type = "Maison";
             } else if (rbImmeuble.isSelected()) {
                 type = "Immeuble";
@@ -114,6 +99,7 @@ public class Projetbatiment extends JFrame {
             }
         }
     }
+
     private class SaveButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -136,25 +122,21 @@ public class Projetbatiment extends JFrame {
                 int nbPieces = Integer.parseInt(nbPiecesStr);
                 for (int i = 0; i < nbPieces; i++) {
                     JOptionPane.showMessageDialog(null, "Configuration de la pièce numéro " + (i + 1) + " au niveau " + (k + 1));
-                    // là on récupère les infos pour les murs
                     List<Mur> murs = new ArrayList<>();
                     for (int j = 0; j < 4; j++) {
                         detmur(murs, i, j);
                     }
-                    // là on récupère les infos pour le sol
                     int solId = detsol(i, murs);
-                    // là on récupère les infos pour le plafond
                     int plafondId = detplafond(i, murs);
 
-                    Piece piece = new Piece(i, solId, plafondId, 2.5, murs); // Assumption: fixed height
+                    Piece piece = new Piece(i, solId, plafondId, 2.5, murs);
                     pieces.add(piece);
                 }
             } catch (NumberFormatException ex) {
                 JOptionPane.showMessageDialog(null, "Veuillez entrer un nombre valide de pièces.", "Erreur", JOptionPane.ERROR_MESSAGE);
             }
         }
-         
-        }
+    }
 
     private void detmur(List<Mur> murs, int i, int j) {
         String debutXi = JOptionPane.showInputDialog(null, "Mur " + (i + 1) + " " + (j + 1) + ": Entrez les coordonnées de début (x):");
@@ -176,7 +158,6 @@ public class Projetbatiment extends JFrame {
             mur.setNbrePortes(nbPortes);
             mur.setNbreFenetres(nbFenetres);
 
-            //choix du revtmt
             uniRevetement[] revetements = Revetement.remplirRevetements();
             uniRevetement revetementChoi = choiRev(revetements, "mur");
 
@@ -210,8 +191,7 @@ public class Projetbatiment extends JFrame {
             double surfaceSol = sol.surface();
             double coutSol = surfaceSol * revetementChoi.getPrixm2();
             coutTotal += coutSol;
-            //JOptionPane.showMessageDialog(null, "Revêtement choisi pour le sol: " + revetementChoi.getNom() + ", Surface du sol: " + surfaceSol + " m², Coût: " + coutSol + "€");
-           
+
             ctparRevetement.merge(revetementChoi.getNom(), coutSol, Double::sum);
             stparRevetement.merge(revetementChoi.getNom(), surfaceSol, Double::sum);
             return revetementChoi.getId();
@@ -230,7 +210,6 @@ public class Projetbatiment extends JFrame {
             double surfacePlafond = plafond.surface();
             double coutPlafond = surfacePlafond * revetementChoi.getPrixm2();
             coutTotal += coutPlafond;
-            //JOptionPane.showMessageDialog(null, "Revêtement choisi pour le plafond: " + revetementChoi.getNom() + ", Surface du plafond: " + surfacePlafond + " m², Coût: " + coutPlafond + "€");
 
             ctparRevetement.merge(revetementChoi.getNom(), coutPlafond, Double::sum);
             stparRevetement.merge(revetementChoi.getNom(), surfacePlafond, Double::sum);
@@ -267,16 +246,17 @@ public class Projetbatiment extends JFrame {
     }
 
     private void sauvegardeDev() {
+        String type = rbMaison.isSelected() ? "Maison" : rbImmeuble.isSelected() ? "Immeuble" : "Inconnu";
         try (FileWriter writer = new FileWriter("devis.txt")) {
             writer.write("Devis pour le projet de construction\n\n");
-            writer.write("Type de construction: " + typeconstruc.getSelectedItem() + "\n");
+            writer.write("Type de construction: " + type + "\n");
             writer.write("Nombre de niveaux: " + nbNiveaux + "\n");
             writer.write("Coût total global: " + coutTotal + " €\n");
             writer.write("Détails des coûts et surfaces par revêtement:\n");
             for (String revName : ctparRevetement.keySet()) {
                 writer.write("Revêtement: " + revName + ", Coût Total: " + ctparRevetement.get(revName) + "€, Surface Totale: " + stparRevetement.get(revName) + " m²\n");
             }
-            //JOptionPane.showMessageDialog(this, "Devis sauvegardé dans 'devis.txt'.");
+            JOptionPane.showMessageDialog(this, "Devis sauvegardé dans 'devis.txt'.");
         } catch (IOException e) {
             JOptionPane.showMessageDialog(this, "Erreur lors de la sauvegarde du devis.", "Erreur", JOptionPane.ERROR_MESSAGE);
         }
@@ -288,7 +268,7 @@ public class Projetbatiment extends JFrame {
             for (Piece piece : pieces) {
                 writer.write(piece.toString() + "\n");
             }
-            //JOptionPane.showMessageDialog(this, "Informations sur le bâtiment sauvegardées dans 'informations_batiment.txt'.");
+            JOptionPane.showMessageDialog(this, "Informations sur le bâtiment sauvegardées dans 'informations_batiment.txt'.");
         } catch (IOException e) {
             JOptionPane.showMessageDialog(this, "Erreur lors de la sauvegarde des informations du bâtiment.", "Erreur", JOptionPane.ERROR_MESSAGE);
         }
@@ -309,4 +289,3 @@ public class Projetbatiment extends JFrame {
         });
     }
 }
-
